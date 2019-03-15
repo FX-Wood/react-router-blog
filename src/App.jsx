ccf14207bfe333023293a173563796607087f97e
FX-Wood <fxwood2@gmail.com>
2019-03-15 16:22:24 -0700

    Added posts from json placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Nav from './Nav'
 import Homepage from './Homepage';
@@ -9,13 +10,24 @@ import FavoriteFood from './FavoriteFood';
 import AboutPage from './AboutPage';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => this.setState({posts: res.data}))
+  }
   render() {
+    console.log('rendering app', this.state)
     return (
       <Router>
         <div className="App">
           <Nav />
           <Route exact path="/" component={Homepage} />
-          <Route path="/blog/" component={MainBlog} />
+          <Route path="/blog/" render={props => <MainBlog posts={this.state.posts} />} />
           <Route path="/movie/" component={FavoriteMovie} />
           <Route path="/food/" component={FavoriteFood} />
           <Route path="/about/" component={AboutPage} />
--- src/MainBlog.jsx
+++ src/MainBlog.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-export default function MainBlog(props) {
+export default function MainBlog({posts}) {
     return (
         <div className="MainBlog">
         <h2>This is the main blog</h2>
             <ul>
-                <li>mainblog</li>
-                <li>mainblog</li>
-                <li>mainblog</li>
-                <li>mainblog</li>
-                <li>mainblog</li>
-                <li>mainblog</li>
+                {posts.map(post => {
+                    return (
+                        <li key={post.id}>
+                            <div className="post">
+                                <h4 className="postTitle">{post.title}</h4>
+                                <p className="postBody">{post.body}</p>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
