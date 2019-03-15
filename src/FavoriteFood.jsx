c50765101af90b498eb1ac80942028b1cb57b1ed
FX-Wood <fxwood2@gmail.com>
2019-03-15 15:44:13 -0700

    Hooked up FavoriteFood

---
+++ src/FavoriteFood.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function FavoriteFood(props) {
+    return (
+        <div className="FavoriteFood">
+            <h2>This is my favorite food</h2>
+            <img src="https://via.placeholderimage.com/150" alt="my favorite food"/>
+            <h3>Recipe</h3>
+            <ul>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+                <li>Ingredient</li>
+            </ul>
+            <p>asdjflkasdjflaksdjflaksjdflkasjdl;fasdlfkjasldkfjasl;kdfjlaskdjflaksdjflaksdjlfkajsdlfkjasdlkfjaslkdjflkasdjflka
+                sjdf;lkajsdlkfjasldkfja;lsdkjfl;kasdjflkasjdflaskd</p>
+        </div>
+    )
+}
