3db8aeec1f1dfa6d4b494dbe6d21d862d70e1573
FX-Wood <fxwood2@gmail.com>
2019-03-15 15:26:09 -0700

    Hooked up homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Homepage from './Homepage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Homepage />
       </div>
     );
   }
---
+++ src/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Homepage(props) {
+    return (
+        <div className="homepage">
+            <h1>Hello! Welcome to the homepage.</h1>
+            <ul>
+                <li>Post</li>
+                <li>Post</li>
+                <li>Post</li>
+                <li>Post</li>
+                <li>Post</li>
+                <li>Post</li>
+                <li>Post</li>
+            </ul>
+        </div>
+    )
+}
