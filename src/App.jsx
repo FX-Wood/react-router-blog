57e95ab8161db25023ba254c0c550c99fab4506f
FX-Wood <fxwood2@gmail.com>
2019-03-15 15:29:41 -0700

    Hooked up Main Blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Homepage from './Homepage'
+import Homepage from './Homepage';
+import MainBlog from './MainBlog'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Homepage />
+        <MainBlog />
       </div>
     );
   }
---
+++ src/MainBlog.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function MainBlog(props) {
+    return (
+        <div className="MainBlog">
+        <h2>This is the main blog</h2>
+            <ul>
+                <li>mainblog</li>
+                <li>mainblog</li>
+                <li>mainblog</li>
+                <li>mainblog</li>
+                <li>mainblog</li>
+                <li>mainblog</li>
+            </ul>
+        </div>
+    )
+}
